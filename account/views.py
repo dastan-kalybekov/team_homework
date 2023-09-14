600ebe762ff0bbbf6b0348dee2c810eeb1be871f
unknown <dakedak670@gmail.com>
2023-09-14 15:30:43 +0600

    added view user

--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import status
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import User
+from .serializer import
+class UserListCreateApiView(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class =
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [AllowAny]
--- post/views.py
+++ post/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import status
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import Post, Comment, Rating
+from .serializer import
+
+class Post(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class =
+    permission_classes = []
