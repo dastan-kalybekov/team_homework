11a20c01f48f411504b020b997a3c6ba0f641402
unknown <dakedak670@gmail.com>
2023-09-14 19:04:52 +0600

    fixed bugs

--- account/serializers.py
+++ account/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
 from .models import User
 
@@ -11,20 +10,6 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'password', 'telegram_chat_id']
-<<<<<<< HEAD
-=======
-    #
-    # def validate_password(self, attrs):
-    #     if len(attrs['password']) < 10:
-    #         raise serializers.ValidationError('длина пароля должна быть не менее 10 символов')
-    #     if not any(c.isdigit()
-    #                for c in attrs['password']):
-    #         raise serializers.ValidationError('пароль должен содержать минимум 1 цифру')
-    #     if not any(c.isalpha()
-    #                for c in attrs['password']):
-    #         raise serializers.ValidationError('пароль должен содержать минимум 1 букву')
-    #     return attrs
->>>>>>> 3cecb1452812b2c240346eaee15d8f4353ee47f0
 
     def create(self, validated_data):
         user = User(
@@ -34,7 +19,3 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
-<<<<<<< HEAD
-=======
-
->>>>>>> 3cecb1452812b2c240346eaee15d8f4353ee47f0
